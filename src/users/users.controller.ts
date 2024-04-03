07d71769b072c593fdf4211c6263ab600fb05c73
Nikunja-Sarma <154231867+Nikunja-Sarma@users.noreply.github.com>
2024-04-03 17:48:09 +0530

    Lesson002

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { query } from 'express';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+  @Get() //GET /users or /user?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get('intern')
+  findAllIntern() {
+    return [];
+  }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() //POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') //PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //DELETE /users/:id
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
